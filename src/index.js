d35424bc4f02a306b9a32632bc162ba6c18bcceb
Nico Schlömer <nico.schloemer@gmail.com>
2015-05-15 12:26:52 +0200

    add module.exports

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 'use strict';
 
-(function() {
+var test = 'http://arxiv.org/abs/1208.0264';
 
-  var test = 'http://arxiv.org/abs/1208.0264';
+//console.log(verifyUrl(test));
+var idRegExp = '(\\d+\\.\\d+|.+/\\d+)(v(\\d+))?';
+var sources = [
+  {
+    name: 'arxiv',
+    regexp: new RegExp(
+      '^(https?://)?(.*\\.)?arxiv.org/(abs|pdf)/(' + idRegExp +
+      ')(\\.pdf)?([#\\?].*)?$',
+      'i'
+    )
+  }
+];
 
-  //console.log(verifyUrl(test));
-  var idRegExp = '(\\d+\\.\\d+|.+/\\d+)(v(\\d+))?';
-  var sources = [
-    {
-      name: 'arxiv',
-      regexp: new RegExp(
-        '^(https?://)?(.*\\.)?arxiv.org/(abs|pdf)/(' + idRegExp +
-        ')(\\.pdf)?([#\\?].*)?$',
-        'i'
-      )
+var verifyUrl = function(url) {
+  for (var i = 0; i < sources.length; i++) {
+    var result = sources[i].regexp.exec(url);
+    if (result) {
+      return result;
     }
-  ];
+  }
+  return undefined;
+};
 
-  var verifyUrl = function(url) {
-    for (var i = 0; i < sources.length; i++) {
-      var result = sources[i].regexp.exec(url);
-      if (result) {
-        return result;
-      }
-    }
-    return undefined;
-  };
-
-})();
+module.exports = {
+  verifyUrl: verifyUrl
+};
